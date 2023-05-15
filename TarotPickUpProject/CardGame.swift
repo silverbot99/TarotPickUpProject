44d9b939b7375648baa5e50b1308dab02a5bd761
silverbot <nhanhuynh.kim23@gmail.com>
2023-05-15 23:26:52 +0700

    first commit

--- TarotPickUpProject/CardGame.swift
+++ TarotPickUpProject/CardGame.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum Rank {
+enum Rank: CaseIterable {
  case Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Page, Knight, Queen, King
     
     func text() -> String {
@@ -15,7 +15,7 @@ enum Rank {
     }
 }
 
-enum Suit {
+enum Suit: CaseIterable {
     case Wand, Cup, Pentacle, Sword, Main
     
     func text() -> String {
@@ -23,15 +23,15 @@ enum Suit {
     }
 }
 
-enum MainCard {
+enum MainCard: CaseIterable {
     case TheFool_0, TheMagician_I, TheHighPriestess_II, TheEmpress_III, TheEmperor_IV, TheHierophant_V, TheLovers_VI
     case TheChariot_VII, Strength_VIII, TheHermit_IX, WheelOfFortune_X, Justice_XI, TheHangedMan_XII, Death_XIII
     case Temperance_XIV, Devil_XV, TheTower_XVI, TheStar_XVII, TheMoon_XVIII, TheSun_XIX, Jubgement_XX, TheWorld_XXI
     
     func text() -> String {
-        if let index = "\(self)".firstIndex(of: "_") {
-            let character = "\(self)"[index]
-            return "\(character)"
+        let arr = "\(self)".split(separator: "_")
+        if arr.count > 0 {
+            return String(arr[arr.count - 1])
         }
         return ""
     }
--- TarotPickUpProject/MainView.swift
+++ TarotPickUpProject/MainView.swift
@@ -8,43 +8,97 @@
 import SwiftUI
 
 struct MainView: View {
-    let cards = [
-        Card(suit: Suit.Main, main: MainCard.Death_XIII),
-        Card(suit: Suit.Main, main: MainCard.TheMoon_XVIII),
-        Card(suit: Suit.Main, main: MainCard.TheSun_XIX),
-        Card(rank: Rank.Ace, suit: Suit.Cup),
 
-    ]
+    var cards : [Card] = {
+        let allMainCard = MainCard.allCases
+        let allRanks = Rank.allCases
+        var main = allMainCard.map { mainCard in
+            Card(suit: Suit.Main, main: mainCard)
+        }
+        let cups = allRanks.map { rank in
+            Card(rank: rank, suit: Suit.Cup)
+        }
+        let pentancles = allRanks.map { rank in
+            Card(rank: rank, suit: Suit.Pentacle)
+        }
+        let swords = allRanks.map { rank in
+            Card(rank: rank, suit: Suit.Sword)
+        }
+        let wands = allRanks.map { rank in
+            Card(rank: rank, suit: Suit.Wand)
+        }
+        var result: [Card] = []
+//        result.append(contentsOf: main)
+//        result.append(contentsOf: cups)
+//        result.append(contentsOf: pentancles)
+//        result.append(contentsOf: swords)
+        result.append(contentsOf: wands)
+        return result
+    }()
+    
+    @State var chosenCard: Card = Card(suit: .Main)
+    @State private var offsetY: CGFloat = 0
+
     var body: some View {
         VStack {
-            horScrollCard
-            //verScrollCard
-        }
-        .padding()
-    }
-    
-    var horScrollCard: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack {
-                ForEach(0..<cards.count, id: \.self) { index in
-                    getCard(cards[index])
-                        .aspectRatio(2/3, contentMode: .fit)
-
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100), spacing: -76)]) {
+                    
+                    ForEach(0..<cards.count, id: \.self) { i in
+                        
+                        Button {
+                            chosenCard = cards[i]
+                            print("card: \(cards[i])")
+                        }label: {
+                            CardView(card: cards[i])
+                                .offset(y: offsetY)
+                                .gesture(
+                                    DragGesture()
+                                        .onChanged { value in
+                                            // Update the offsetY based on the drag translation
+                                            offsetY = value.translation.height
+                                        }
+                                        .onEnded { _ in
+                                            // Reset the offsetY after the drag ends
+                                            offsetY = 0
+                                        }
+                                )
+                        }
+                    }
                 }
+
             }
 
-        }
+            //verScrollCard
+            
+            Spacer()
 
+            CardView(card: chosenCard)
+                
+            Spacer()
+        }
+        .padding()
     }
     var verScrollCard: some View {
         ScrollView(.vertical, showsIndicators: false) {
             ForEach(0..<cards.count, id: \.self) { index in
-                getCard(cards[index])
+                Button {
+                    chosenCard = cards[index]
+                }label: {
+                    CardView(card: cards[index])
+                }
             }
         }
     }
     
-    func getCard(_ card: Card) -> some View {
+    
+}
+
+struct CardView: View {
+    let card: Card
+    
+    var body: some View {
+        
         VStack {
             
             Image(card.filename)
@@ -58,9 +112,9 @@ struct MainView: View {
                 .fill(Color.white)
                 .shadow(radius: 2)
         )
+        .aspectRatio(2/3, contentMode: .fit)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
